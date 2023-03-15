7a8898cfe71e7d91c496f504e5288ced77af5a14
Phil Oxrud <philoxrud@gmail.com>
2023-03-15 10:45:36 -0400

    feat(user-profile): implement UserFeedPage to use Cognito

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -1,14 +1,14 @@
-import './UserFeedPage.css';
+import "./UserFeedPage.css";
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import DesktopNavigation  from '../components/DesktopNavigation';
-import DesktopSidebar     from '../components/DesktopSidebar';
-import ActivityFeed from '../components/ActivityFeed';
-import ActivityForm from '../components/ActivityForm';
+import DesktopNavigation from "../components/DesktopNavigation";
+import DesktopSidebar from "../components/DesktopSidebar";
+import ActivityFeed from "../components/ActivityFeed";
+import ActivityForm from "../components/ActivityForm";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from "aws-amplify";
 
 export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -21,45 +21,56 @@ export default function UserFeedPage() {
 
   const loadData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${title}`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${title}`;
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setActivities(resJson)
+        setActivities(resJson);
       } else {
-        console.log(res)
+        console.log(res);
       }
     } catch (err) {
       console.log(err);
     }
   };
 
+  // check if we are authenicated
   const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
-      setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false.
+      // If set to true, this call will send a
+      // request to Cognito to get the latest user data
+      bypassCache: false
+    })
+      .then((cognito_user) => {
+        setUser({
+          display_name: cognito_user.attributes.name,
+          handle: cognito_user.attributes.preferred_username
+        });
       })
-    }
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
     loadData();
     checkAuth();
-  }, [])
+  }, []);
 
   return (
     <article>
-      <DesktopNavigation user={user} active={'profile'} setPopped={setPopped} />
-      <div className='content'>
+      <DesktopNavigation user={user} active={"profile"} setPopped={setPopped} />
+      <div className="content">
         <ActivityForm popped={popped} setActivities={setActivities} />
         <ActivityFeed title={title} activities={activities} />
       </div>
