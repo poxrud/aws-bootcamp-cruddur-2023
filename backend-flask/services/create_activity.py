3fe3bd017769face32248341b39c7e814b3a93c3
Phil Oxrud <philoxrud@gmail.com>
2023-03-23 21:27:27 +0000

    WIP: activities

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,5 +1,9 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+
+from lib.db import db
+
+
 class CreateActivity:
   def run(message, user_handle, ttl):
     model = {
@@ -7,22 +11,24 @@ class CreateActivity:
       'data': None
     }
 
+    user_uuid = ""
+
     now = datetime.now(timezone.utc).astimezone()
 
     if (ttl == '30-days'):
-      ttl_offset = timedelta(days=30) 
+      ttl_offset = timedelta(days=30)
     elif (ttl == '7-days'):
-      ttl_offset = timedelta(days=7) 
+      ttl_offset = timedelta(days=7)
     elif (ttl == '3-days'):
-      ttl_offset = timedelta(days=3) 
+      ttl_offset = timedelta(days=3)
     elif (ttl == '1-day'):
-      ttl_offset = timedelta(days=1) 
+      ttl_offset = timedelta(days=1)
     elif (ttl == '12-hours'):
-      ttl_offset = timedelta(hours=12) 
+      ttl_offset = timedelta(hours=12)
     elif (ttl == '3-hours'):
-      ttl_offset = timedelta(hours=3) 
+      ttl_offset = timedelta(hours=3)
     elif (ttl == '1-hour'):
-      ttl_offset = timedelta(hours=1) 
+      ttl_offset = timedelta(hours=1)
     else:
       model['errors'] = ['ttl_blank']
 
@@ -30,22 +36,39 @@ class CreateActivity:
       model['errors'] = ['user_handle_blank']
 
     if message == None or len(message) < 1:
-      model['errors'] = ['message_blank'] 
+      model['errors'] = ['message_blank']
     elif len(message) > 280:
-      model['errors'] = ['message_exceed_max_chars'] 
+      model['errors'] = ['message_exceed_max_chars']
 
     if model['errors']:
       model['data'] = {
-        'handle':  user_handle,
+        'handle': user_handle,
         'message': message
-      }   
+      }
     else:
+      self.create_activity()
       model['data'] = {
         'uuid': uuid.uuid4(),
         'display_name': 'Andrew Brown',
-        'handle':  user_handle,
+        'handle': user_handle,
         'message': message,
         'created_at': now.isoformat(),
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+
+
+def create_activity(handle, message, expires_at):
+  sql = """
+  insert into (
+    user_uuid,
+    message,
+    expires_at
+  )
+  values (
+    (select uuid from public.users where users.handle = %(handle)s limit 1), 
+    %(user_uuid)s, 
+    %(message)s, 
+    %(expires_at)s) returning uuid;
+  """
+  uuid = db.query_commit_with_returning_id(sql, handle, message, expires_at)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from utils.logger import LOGGER
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 from opentelemetry import trace
 
@@ -18,7 +18,7 @@ class HomeActivities:
       span.set_attribute("app.userId", 1001)
       span.set_attribute("app.now", now.isoformat())
 
-      sql = query_wrap_array("""
+      results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -34,13 +34,4 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
       """)
-
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-
-      return json[0]
-      
+      return results
