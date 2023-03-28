cbf3fc7bba34582e91250847e69f3fabb15da383
Phil Oxrud <philoxrud@gmail.com>
2023-03-28 16:14:38 +0000

    feat: makes sql logs show the parameters in statements

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -38,14 +38,14 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self, title, sql):
+  def print_sql(self, title, sql, params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql, params)
 
   def query_commit(self, sql, params={}):
-    self.print_sql('commit with returning', sql)
+    self.print_sql('commit with returning', sql, params)
 
     pattern = re.compile(r"\bRETURNING\b", re.IGNORECASE)
     is_returning_id = pattern.search(sql)
@@ -67,7 +67,7 @@ class Db:
   # when we want to return a json object
 
   def query_array_json(self, sql, params={}):
-    self.print_sql('array', sql)
+    self.print_sql('array', sql, params)
 
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -79,7 +79,7 @@ class Db:
 
   def query_object_json(self, sql, params={}):
 
-    self.print_sql('json', sql)
+    self.print_sql('json', sql, params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
 
@@ -91,6 +91,14 @@ class Db:
           "{}"
         else:
           return json[0]
+
+  def query_value(self, sql, params={}):
+    self.print_sql('value', sql, params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql, params)
+        json = cur.fetchone()
+        return json[0]
 
   def query_wrap_object(self, template):
     sql = f"""
