b9ab54da7ef1849c0c7ffb99cbe3586dca879b5f
Phil Oxrud <philoxrud@gmail.com>
2023-03-29 16:06:51 +0000

    feat: add bin scripts for updating cognito uuid in RDS database

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,9 +2,18 @@
 INSERT INTO
   public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'anrew@exampro.co', 'andrewbrown', 'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko', 'MOCK');
-
+  (
+    'Phil Oxrud',
+    'philoxrud@gmail.com',
+    'philoxrud',
+    'MOCK'
+  ),
+  (
+    'John Smith',
+    'philoxrud+johnsmith@gmail.com',
+    'johnsmith',
+    'MOCK'
+  );
 INSERT INTO
   public.activities (user_uuid, message, expires_at)
 VALUES
@@ -15,10 +24,8 @@ VALUES
       from
         public.users
       WHERE
-        users.handle = 'andrewbrown'
+        users.handle = 'philoxrud'
       LIMIT
         1
-    ),
-    'This was imported as seed data!',
-    current_timestamp + interval '10 day'
+    ), 'This was imported as seed data!', current_timestamp + interval '10 day'
   )
