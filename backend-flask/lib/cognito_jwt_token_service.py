6e6052e33e965280f58db864b2f92c3546e3d236
Phil Oxrud <philoxrud@gmail.com>
2023-04-01 16:18:54 +0000

    fix: remove debug log

--- backend-flask/lib/cognito_jwt_token_service.py
+++ backend-flask/lib/cognito_jwt_token_service.py
@@ -14,9 +14,6 @@ class CognitoJwtToken:
       response = requests.get('http://verify-cognito-token:3050/verify-cognito-token',
                               headers={'Authorization': f'{auth_header}'})
 
-      LOGGER.debug("AUTHHEADER:")
-      LOGGER.debug(response.status_code)
-
       if response.status_code == 401:
         raise TokenVerifyError("unathenticated")
 
