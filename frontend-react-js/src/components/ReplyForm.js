e2bca63565aecaa4d8d9be09a4a50b6a1e305969
Phil Oxrud <philoxrud@gmail.com>
2023-07-18 14:44:03 +0000

    Implement Replies for Posts

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,7 +1,7 @@
 import './ReplyForm.css';
 import React from "react";
 import process from 'process';
-// import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import { getAccessToken } from "../lib/CheckAuth";
 
 import ActivityContent from '../components/ActivityContent';
 
@@ -19,13 +19,16 @@ export default function ReplyForm(props) {
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+      const access_token = await getAccessToken();
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          Authorization: `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+          activity_uuid: props.activity.uuid,
           message: message
         }),
       });
